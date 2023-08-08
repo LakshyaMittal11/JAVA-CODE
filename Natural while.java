82e6322d354cdbaba11178e4475b3cb6f8023e5e
LakshyaMittal11 <122609180+LakshyaMittal11@users.noreply.github.com>
2023-08-08 16:39:20 +0530

    Add files via upload

---
+++ Natural while.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Naturalwhile
+{
+public static void main(String K[])
+{
+Scanner ob=new Scanner(System.in);
+System.out.print("enter the no.=");
+int a=ob.nextInt();
+int i=1;
+while(i<=a)
+{
+System.out.println(i);
+i++
+}
+}
---
+++ Natural.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Natural 
+{
+public static void main(String K[])
+{
+Scanner ob=new Scanner(System.in);
+System.out.print("enter the no.=");
+int a=ob.nextInt();
+int i;
+for(i=1;i<=a;i++)
+System.out.println(i);
+}
+}
+
+
