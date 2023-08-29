edff437ce07d6170fbd5b6ed6a58630c141c2294
LakshyaMittal11 <122609180+LakshyaMittal11@users.noreply.github.com>
2023-08-29 22:41:18 +0530

    Add files via upload

---
+++ Maximum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Maximum
+{
+    public static void main(String K[])
+    {
+        Scanner ob=new Scanner(System.in);
+        System.out.print("enter the 2 number");
+        int a=ob.nextInt();
+        int b=ob.nextInt();
+        if(a>b)
+        System.out.println("a is maximum no.");
+        else
+        System.out.println("b is maximum");
+    }
+}
