507a14ab76e591d8ceddde5421df2d61a10ad8e2
LakshyaMittal11 <122609180+LakshyaMittal11@users.noreply.github.com>
2023-09-06 23:35:39 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class table
+{
+public static void main(String K[])
+{
+Scanner ob= new Scanner(System.in);
+System.out.print("enter the no.=");
+int n=ob.nextInt();
+int i;
+for(i=1;i<=10;i++)
+{
+System.out.println(n*i);
+}
+}
+}
