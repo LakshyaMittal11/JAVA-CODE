7d953a5d82420d0370035f462877cccd1d201d2a
LakshyaMittal11 <122609180+LakshyaMittal11@users.noreply.github.com>
2023-09-12 19:14:24 +0530

    Add files via upload

---
+++ Z.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Z
+{
+public static void main(String k[])
+{
+int i,j;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<+5;j++)
+{
+if(i==1||i==5||(i+j)==6)
+System.out.print("*");
+else 
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+}
