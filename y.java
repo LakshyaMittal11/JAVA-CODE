3901167386c0bb5016931e761f7e596a81d72a73
LakshyaMittal11 <122609180+LakshyaMittal11@users.noreply.github.com>
2023-09-20 23:16:50 +0530

    Add files via upload

---
+++ y.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class y
+{
+public static void main(String K[])
+{
+Scanner ob=new Scanner(System.in);
+int a,b,c,d;
+System.out.println("enter three no.");
+a=ob.nextInt();
+b=ob.nextInt();
+c=ob.nextInt();
+d=a+b+c/3;
+System.out.println("average="+d);
+}
+}
