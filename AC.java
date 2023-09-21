95b5e3b01ddcf305d99b720f66e172a5834c6d2b
LakshyaMittal11 <122609180+LakshyaMittal11@users.noreply.github.com>
2023-09-21 22:53:50 +0530

    Add files via upload

---
+++ AC.java
@@ -0,0 +1,15 @@
+//area and circumference of circle
+import java.util.*;
+class AC
+{
+public static void main(String K[])
+{
+Scanner ob=new Scanner(System.in);
+System.out.print("enter the no.=");
+double r=ob.nextInt();
+double a=3.14*r*r;
+double c=2*3.14*r;
+System.out.println("area of circle"+a);
+System.out.println("circumference of circle"+c);
+}
+}
